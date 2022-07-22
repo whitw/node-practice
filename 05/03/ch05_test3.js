e6a4a2401924aa9fa0d99345d038b12a00faa3f1
whitw <shkim7876@naver.com>
2022-07-22 13:58:59 +0000

    sending file to

--- 05/03/ch05_test3.js
+++ 05/03/ch05_test3.js
@@ -1,11 +1,13 @@
-var http = request('http');
-var fs = request('fs');
+var http = require('http');
+var fs = require('fs');
 
-var server = createServer(function(){});
+var server = http.createServer(function(){});
 var port = 3000;
 
+//sending file
+
 server.on("request", function(req, res){
-	var filename = 'house.png';
+	var filename = 'owo.png';
 	fs.readFile(filename, function(err, data) {
 		res.writeHead(200, {"Content-Type":"image/png"});
 		res.write(data);
